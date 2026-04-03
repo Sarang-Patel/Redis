f8b378df14a39f92907db36f41d58a604b0e7fd2
Sarang Ajay Patel <sarangp203@gmail.com>
2026-04-02 22:11:37 -0400

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,8 @@ import java.net.Socket;
 public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
-
+    System.out.println("Logs from your program will appear here!"); 
+ 
     //  Uncomment the code below to pass the first stage
     //    ServerSocket serverSocket = null;
     //    Socket clientSocket = null;
